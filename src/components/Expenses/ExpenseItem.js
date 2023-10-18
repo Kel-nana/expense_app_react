a60c2e645b7b5ef0f877f41b90f9317c67ecdf2e
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-10-18 17:10:11 +0300

    Code clean up

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,6 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  // const expenseDate = new Date(2023, 6, 6);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = 294.67;
-  // const month = props.date.toLocaleString('en-US', { month: 'long' });
-  // const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  // const year = props.date.getFullYear();
   const [title, setTitle] = useState(props.title);
   console.log("expenseitem evaluated ");
   const clickHandler = () => {

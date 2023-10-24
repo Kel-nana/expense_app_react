7df73bf6230d1c957dd70f77a923762e00a67b54
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-10-24 22:41:18 +0300

    Add  comments expense_items

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,8 +4,10 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  // Define a state variable 'title' and initialize it with the 'title' prop
   const [title, setTitle] = useState(props.title);
 
+  // Event handler for changing the title when the "change" button is clicked
   const clickHandler = () => {
     setTitle("Updated!");
   };

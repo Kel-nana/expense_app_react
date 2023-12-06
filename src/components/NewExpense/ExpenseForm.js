94efce3ed0c0d76280ed8dd2bfe3ccf8195ecd5e
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-12-06 19:47:07 +0300

    button basic structure & styling

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -50,6 +50,7 @@ console.log(openForm, 'open form')
     // console.log(expenseData);
     props.onSaveExpenseData(expenseData); 
     // clear input field
+    
     setUserInput({
       title: "",
       amount: "",

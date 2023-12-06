e8989aff291bef142566c38ccd7b0715331519e7
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-12-06 16:01:19 +0300

    Convert date from string to date format

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
        const expenseDataSubmitedHandler = (enteredExpenseData)=> {
-        //pass in expenseData
+
+        // Convert the date string to a JavaScript Date object
+    const date = new Date(enteredExpenseData.date);
+
+      //pass in expenseData
  const expenseData = {
     ...enteredExpenseData, 
+    date: date,
     id:Math.random().toString()
 }
+props.onAddExpense(expenseData)
 console.log(expenseData, 'new expense data');
        };
     return (

23fd7b1c56297cb166d8360008c8e0e3af79f8d4
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-12-06 15:57:15 +0300

    code clean up

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,23 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   // Define a state variable 'title' and initialize it with the 'title' prop
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
   // Event handler for changing the title when the "change" button is clicked
-  const clickHandler = () => {
-    setTitle("Updated!");
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  // };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button  onClick={clickHandler}>change</button>
     </Card>
   );
 }; 
